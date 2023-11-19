d398276cedbc35e8b9303a85aef0c2dd0c75a0c7
cadrgtsecond <abhinavkrishnacr2020@gmail.com>
2023-11-19 19:57:11 +0530

    feat: server-bpp: Ensure domains are real domains

--- talentz/code/bpp/server-bpp/openai.js
+++ talentz/code/bpp/server-bpp/openai.js
@@ -1,19 +1,20 @@
 // Buffer wrapping is reccommended
 import OpenAI from 'openai';
-import "dotenv/config"
+import "dotenv/config";
+import similarity from 'string-similarity';
+import fs from 'fs';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_SECRET_KEY,
 });
 
-// My prompt engineering is subpar
 const initial_prompt = [
   {role: 'user', content: 'You are a consultant helping small buisinesses such as shops to set up a developer team and build their dream applications, websites etc.'}
 ];
 const model = "gpt-3.5-turbo-1106";
 
 function getQuestion(request) {
-  return `One of our clients has a request. It is given in the next line. Suggest him what all domains he should be looking for when hiring developers to build his dream. Return your response in JSON. Return the various domains in the key "domains":
+  return `One of our clients has a request. It is given in the next line. Suggest him what all domains/skills he should be looking for when hiring developers to build his dream. Return your response in JSON. Return the various domains in the key "domains":
   "${request}"`
 };
 
@@ -38,5 +39,11 @@ export async function getDomains(request) {
   const result = JSON.parse(completion.choices[0].message.content);
 
   // Let's all pray to god ChatGPT doesn't break its "API"
-  return result.domains;
+  return mapToRealDomains(result.domains);
+}
+
+const real_domains = JSON.parse(fs.readFileSync("../../domains.json"));
+function mapToRealDomains(domains) {
+  return domains.map(d =>
+           similarity.findBestMatch(d, real_domains).bestMatch.target)
 }
